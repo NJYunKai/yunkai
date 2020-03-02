f5e10a81ccc7e62d74c12349d6d248d71926c069
zhang-ch961 <zhang_ch961@qq.com>
2020-03-02 17:30:39 +0800

    解决跨域问题

--- src/main.js
+++ src/main.js
@@ -36,7 +36,8 @@ axios.defaults.timeout = 5000
 //全局定义axios
 // axios.defaults.baseURL = 'http://shkjgw.shkjem.com/api/'
 //本地测试地址
-axios.defaults.baseURL = ' http://localhost:8081/website/interface/'
+// axios.defaults.baseURL = ' http://localhost:8081/website/interface/'
+axios.defaults.baseURL = '/website/interface/'
 // http://localhost:8080/website/interface/Cases/GetCasesAll
 Vue.prototype.$http = axios
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 module.exports = {
     // 根路径 默认使用/ vue cli 3.3+ 弃用 baseUrl
-    publicPath: './' // 此处改为 './' 即可
+    publicPath: './', // 此处改为 './' 即可
+    devServer: {
+        open: true, //是否自动弹出浏览器页面
+        host: "localhost", 
+        port: '8081',
+        https: false,
+        hotOnly: false, 
+        proxy: {
+            '/website/interface/': {
+                target: 'http://localhost:8081', //API服务器的地址
+                ws: true,  //代理websockets
+                changeOrigin: true, // 虚拟的站点需要更管origin
+                pathRewrite: {   //重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
+                    // '^/api': '/website/interface/'
+                }
+            }
+        },
+    }
   }
